712d8afee07786fa8765a471596afc609db59a11
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-01-12 09:28:14 +0700

    RN Installation

---
+++ App.js
@@ -0,0 +1,55 @@
+import { StatusBar } from "expo-status-bar";
+
+import { NativeBaseProvider, extendTheme } from 'native-base'
+
+import AppLoading from "expo-app-loading"
+
+import {
+  useFonts, 
+  BalsamiqSans_400Regular, 
+  BalsamiqSans_400Regular_Italic
+} from '@expo-google-fonts/balsamiq-sans'
+
+import FormNativeBase from "./src/screens/form";
+
+export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_400Regular_Italic
+  })
+
+  const fontConfig = {
+    BalsamiqSans: {
+      400: {
+        normal: "BalsamiqSans_400Regular",
+        italic: "BalsamiqSans_400Regular_Italic",
+      }
+    }
+  }
+
+  const costumeColor = {
+    primary: {
+      300: "#67e8f9",
+      600: "#0891b2",
+      900: "#164e63",
+    }
+  }
+
+  const theme = extendTheme({
+    colors: costumeColor,
+    fontConfig,
+    fonts: {
+      heading: "BalsamiqSans",
+      body: "BalsamiqSans",
+      mono: "BalsamiqSans"
+    },
+    config: { initialColorMode: "dark" }
+  })
+
+  return (
+    <NativeBaseProvider>
+      <FormNativeBase />
+    </NativeBaseProvider>
+  );
+}
